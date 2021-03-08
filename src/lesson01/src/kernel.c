9440487f912b1206e949e2615c852a65815d7417
Felix Lin <linxz02@gmail.com>
2021-03-07 23:10:13 -0500

    helloworld

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -3,9 +3,10 @@
 void kernel_main(void)
 {
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
+	uart_send_string("Hello, world - Rpi4!\r\n");
 
 	while (1) {
-		uart_send(uart_recv());
+		// uart_send(uart_recv());
+		;
 	}
 }
