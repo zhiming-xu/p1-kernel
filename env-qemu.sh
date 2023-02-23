fe9903dbbfd816665e8f4dbbe15196e1353bed64
Felix Lin <linxz02@gmail.com>
2023-02-23 15:55:15 +0000

    update env-qemu

--- env-qemu.sh
+++ env-qemu.sh
@@ -12,6 +12,7 @@ run-uart0() {
 run() {
     # xzl the following will launch VNC server w/ binding to a port. in case there are many qemu instancs, it may fail to bind to an addr?
     # qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial stdio
+
     # to avoid, launch w/o graphics...
     echo "**Note: use Ctrl-a then x to terminate QEMU"
     qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial mon:stdio -nographic
@@ -21,8 +22,17 @@ run-mon() {
     qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -monitor stdio
 }
 
+MYGDBPORT=25678
 run-debug() {
-    qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial stdio -s -S
+    # see comment above 
+    # qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial stdio -s -S
+
+    echo "**Note: use Ctrl-a then x to terminate QEMU"
+    echo "	in a separate window, launch gdb: gdb-multiarch build/kernel8.elf"
+    echo "	configure gdb by editing ~/.gdbinit "
+    echo "	details: https://fxlin.github.io/p1-kernel/gdb/"
+    #qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial mon:stdio -nographic -s -S
+    qemu-system-aarch64 -M raspi3 -kernel ./kernel8.img -serial null -serial mon:stdio -nographic -gdb tcp::${MYGDBPORT} -S
 }
 
 run-log() {
